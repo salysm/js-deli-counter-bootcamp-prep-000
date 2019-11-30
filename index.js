4dba210e84dd9a074154cb2ec12f431cd28a1e28
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-30 17:34:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 var katzDeliLine = [];
 
-function currentLine(x) {
-    var line = []
-    if (x.length === 0) {
-      return "The line is currently empty."
-    } else {
-      for(var i = 0; i < x.length; i++) {
-        line += (i + 1) + ". " + x[i] + ", "
-      }
-      line = line.slice(0, line.length-2)
-      return "The line is currently: " + line
-    }
-}
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
-function nowServing(x) {
-  if (x.length === 0) {
+var number = 0
+function takeANumber2(katzDeliLine) {
+number++ 
+katzDeliLine.push(number)
+return "You have ticket number " + number + "."
+}
+
+ 
+function currentLine(NewLine) {
+    var line = []
+    if (NewLine.length === 0) {
+     return "The line is currently empty."
+   } else {
+     for (var i = 0; i < NewLine.length; i++) {
+       line += (i + 1) + ". " + NewLine[i] + ", "
+   }
+   line = line.slice(0, line.length-2)
+   return "The line is currently: " + line
+}
+}
+function nowServing(y) {
+  if (y.length ===0) {
     return "There is nobody waiting to be served!"
   } else {
-    var name = x[0];
-    x.splice(0, 1);
+    var name = y[0];
+    y.splice(0, 1);
     return "Currently serving " + name + ".";
   }
 }
